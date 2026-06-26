4780c143abec4119ce628f2aa9c8c7fa2e67245b
Doeun <doeunkim.cs@gmail.com>
2026-06-26 01:14:13 -0700

    Deploy updated personal website

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,1 +1,87 @@
-$(document).ready((function(){if($("a.abstract").click((function(){$(this).parent().parent().find(".abstract.hidden").toggleClass("open"),$(this).parent().parent().find(".award.hidden.open").toggleClass("open"),$(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open")})),$("a.award").click((function(){$(this).parent().parent().find(".abstract.hidden.open").toggleClass("open"),$(this).parent().parent().find(".award.hidden").toggleClass("open"),$(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open")})),$("a.bibtex").click((function(){$(this).parent().parent().find(".abstract.hidden.open").toggleClass("open"),$(this).parent().parent().find(".award.hidden.open").toggleClass("open"),$(this).parent().parent().find(".bibtex.hidden").toggleClass("open")})),$("a").removeClass("waves-effect waves-light"),$("#toc-sidebar").length){$(".publications h2").each((function(){$(this).attr("data-toc-skip","")}));var e="#toc-sidebar",t=$(e);Toc.init(t),$("body").scrollspy({target:e})}const n=document.createElement("link");n.href="../css/jupyter.css",n.rel="stylesheet",n.type="text/css";let a=determineComputedTheme();$(".jupyter-notebook-iframe-container iframe").each((function(){$(this).contents().find("head").append(n),"dark"==a&&$(this).bind("load",(function(){$(this).contents().find("body").attr({"data-jp-theme-light":"false","data-jp-theme-name":"JupyterLab Dark"})}))})),$('[data-toggle="popover"]').popover({trigger:"hover"})}));
+$(document).ready(function () {
+  // add toggle functionality to abstract, award and bibtex buttons
+  $("a.abstract").click(function () {
+    $(this).parent().parent().find(".abstract.hidden").toggleClass("open");
+    $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+  });
+  $("a.award").click(function () {
+    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".award.hidden").toggleClass("open");
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+  });
+  $("a.bibtex").click(function () {
+    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
+    $(this).parent().parent().find(".bibtex.hidden").toggleClass("open");
+  });
+  $("a").removeClass("waves-effect waves-light");
+
+  // bootstrap-toc
+  if ($("#toc-sidebar").length) {
+    // remove related publications years from the TOC
+    $(".publications h2").each(function () {
+      $(this).attr("data-toc-skip", "");
+    });
+    var navSelector = "#toc-sidebar";
+    var $myNav = $(navSelector);
+    Toc.init($myNav);
+    $("body").scrollspy({
+      target: navSelector,
+    });
+
+    if ($(".cv").length) {
+      var cvHeadings = $(".cv h3[id]");
+      var activateCvToc = function () {
+        var threshold = $(window).scrollTop() + $(".navbar").outerHeight() + 80;
+        var activeId = cvHeadings.first().attr("id");
+
+        cvHeadings.each(function () {
+          if ($(this).offset().top <= threshold) {
+            activeId = $(this).attr("id");
+          }
+        });
+
+        $(navSelector + " .nav-link").removeClass("active");
+        $(navSelector + ' .nav-link[href="#' + activeId + '"]').addClass("active");
+      };
+
+      activateCvToc();
+      $(window).on("scroll resize", activateCvToc);
+      $(navSelector).on("click", ".nav-link", function () {
+        var clickedLink = $(this);
+        window.setTimeout(function () {
+          $(navSelector + " .nav-link").removeClass("active");
+          clickedLink.addClass("active");
+          activateCvToc();
+        }, 0);
+      });
+    }
+  }
+
+  // add css to jupyter notebooks
+  const cssLink = document.createElement("link");
+  cssLink.href = "../css/jupyter.css";
+  cssLink.rel = "stylesheet";
+  cssLink.type = "text/css";
+
+  let jupyterTheme = determineComputedTheme();
+
+  $(".jupyter-notebook-iframe-container iframe").each(function () {
+    $(this).contents().find("head").append(cssLink);
+
+    if (jupyterTheme == "dark") {
+      $(this).bind("load", function () {
+        $(this).contents().find("body").attr({
+          "data-jp-theme-light": "false",
+          "data-jp-theme-name": "JupyterLab Dark",
+        });
+      });
+    }
+  });
+
+  // trigger popovers
+  $('[data-toggle="popover"]').popover({
+    trigger: "hover",
+  });
+});
